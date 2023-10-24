3175178524b92c6d9e204ec9fc4cf52f435f1f28
Nathan Rocha Torrente <nathan.torrente.rocha@gmail.com>
2023-10-24 14:15:13 -0400

    1º Alteração

---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,50 @@
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)// clear button
+        {
+
+
+        }
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
